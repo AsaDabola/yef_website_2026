1aa2167bf0ac31c3808bc9466447f0034444d320
Asa Daboh <160549474+AsaDabola@users.noreply.github.com>
2026-08-28 22:20:32 -0400

    Update historical event description in HistoryTimeline

--- src/components/who-we-are/HistoryTimeline.tsx
+++ src/components/who-we-are/HistoryTimeline.tsx
@@ -145,7 +145,7 @@ const timeline: Entry[] = [
     body: [
       "By 2015, YEF's international mission had expanded to campuses across North America, South America, Europe, Africa, Asia Pacific, Southeast Asia, and South Asia.",
       "This period also marked the beginning of YEF's first Leadership Conference series in Korea, bringing leaders together for biblical training, fellowship, mission planning, and preparation for continued international expansion.",
-      "YEF held the Easter Gathering, Pentecost Gathering, and Christmas Retreat and participated in the 25th General Assembly of KWMA.",
+      "YEF held the Easter Gathering, Pentecost Gathering, and Christmas Retreat and participated in the 25th General Assembly of KWMA. Chris Park",
     ],
   },
   {
